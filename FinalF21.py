8ec977eff48a793de002da7c97ae364262155e86
7Junny7 <72548541+7Junny7@users.noreply.github.com>
2022-03-23 22:31:14 -0700

    Final Project

---
+++ FinalF21.py
@@ -0,0 +1,14 @@
+def main():
+    '''
+    1)  read file and store in your data structure
+	2)	Process data (verify scores, calculate average and assign grade)
+	3)	Print course content to the monitor
+	4)	Search for a student and print details to the monitor
+	5)	Add a new student to the list
+	6)  Remove a student from the list
+	7)	Sort the list of students alphabetically and print to the monitor
+	8)  Print course content to a file
+	'''
+    
+    return
+main()
